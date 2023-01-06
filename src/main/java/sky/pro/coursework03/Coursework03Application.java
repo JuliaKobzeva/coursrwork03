2371aab06657c741b7ec1dc73add7c3ea626dd07
Юля Кобзева <selemina@mail.ru>
2023-01-06 11:57:18 +0300

    первый коммит

---
+++ src/main/java/sky/pro/coursework03/Coursework03Application.java
@@ -0,0 +1,13 @@
+package sky.pro.coursework03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework03Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/coursework03/Coursework03ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.coursework03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
